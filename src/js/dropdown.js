13f64cb71d1b46572a2824700c16fa14896adba9
Ralfik <developeraul@gmail.com>
2022-02-25 19:44:15 +0500

    сверстаны блоки от 1 до 5 и дропдаун

---
+++ src/js/dropdown.js
@@ -0,0 +1,111 @@
+class Dropdown {
+  constructor(name) {
+    const listItems = document
+      .getElementById(`dropdownList_${name}`)
+      .getElementsByTagName("li");
+    const dropDownHeader = document.getElementById(`dropdownHeader_${name}`);
+    const chevronIcon = document.getElementById(`chevronIcon_${name}`);
+    const dropdownListBody = document.getElementById(
+      `dropdownListBody_${name}`
+    );
+    const dropdownHeaderValue = document.getElementById(
+      `dropdownHeaderValue_${name}`
+    );
+    const isDropdownDisabled = document
+      .getElementById(`dropdown_${name}`)
+      .classList.contains("disabled");
+    const dropdown = document.getElementById(`dropdown_${name}`);
+    Array.from(listItems).forEach((item) => {
+      item.addEventListener(
+        "click",
+        function () {
+          toggleInterface();
+          selectItem(this);
+        },
+        false
+      );
+    });
+    dropDownHeader.addEventListener(
+      "click",
+      function () {
+        if (!isDropdownDisabled) {
+          toggleInterface();
+        }
+      },
+      false
+    );
+    function toggleInterface() {
+      chevronIcon.classList.toggle("rotate-icon-home");
+      chevronIcon.classList.toggle("rotate-icon");
+      dropdownListBody.classList.toggle("dropdown__body--hide");
+      dropdownListBody.classList.toggle("dropdown__body--show");
+      // dropdownHeader.classList.toggle("dropdown__header--hide");
+      // dropdownHeader.classList.toggle("dropdown__header--show");
+    }
+    function selectItem(_this) {
+      dropdownHeaderValue.classList.add("dropdown__header--active");
+      dropdownHeaderValue.innerText = _this.innerText;
+      Array.from(listItems).forEach((item) => {
+        item.classList.remove("selected");
+      });
+      _this.classList.add("selected");
+    }
+  }
+}
+new Dropdown("1");
+new Dropdown("2");
+new Dropdown("3");
+// new Dropdown("4");
+
+// const listItems = document
+//   .getElementById("dropdownList")
+//   .getElementsByTagName("li");
+// const dropDownHeader = document.getElementById("dropdownHeader");
+// const chevronIcon = document.getElementById("chevronIcon");
+// const dropdownListBody = document.getElementById("dropdownListBody");
+// const dropdownHeaderValue = document.getElementById("dropdownHeaderValue");
+// const isDropdownDisabled = document
+//   .getElementById("dropdown")
+//   .classList.contains("disabled");
+// const dropdown = document.getElementById("dropdown");
+
+// Array.from(listItems).forEach((item) => {
+//   item.addEventListener(
+//     "click",
+//     function () {
+//       toggleInterface();
+//       selectItem(this);
+//     },
+//     false
+//   );
+// });
+
+// dropDownHeader.addEventListener(
+//   "click",
+//   function () {
+//     if (!isDropdownDisabled) {
+//       toggleInterface();
+//     }
+//   },
+//   false
+// );
+
+// function toggleInterface() {
+//   chevronIcon.classList.toggle("rotate-icon-home");
+//   chevronIcon.classList.toggle("rotate-icon");
+//   dropdownListBody.classList.toggle("dropdown__body--hide");
+//   dropdownListBody.classList.toggle("dropdown__body--show");
+//   dropdownHeader.classList.toggle("dropdown__header--hide");
+//   dropdownHeader.classList.toggle("dropdown__header--show");
+// }
+
+// function selectItem(_this) {
+//   dropdownHeaderValue.classList.add("dropdown__header--active");
+//   dropdownHeaderValue.innerText = _this.innerText;
+
+//   Array.from(listItems).forEach((item) => {
+//     item.classList.remove("selected");
+//   });
+
+//   _this.classList.add("selected");
+// }
--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,3 @@
 import "../scss/main.scss";
+import "./dropdown";
 import "../index.html";
