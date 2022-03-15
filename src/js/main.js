30f3bcbd8e01b3e2fc3d4909aa3a13aea4159fc6
Ralfik <developeraul@gmail.com>
2022-03-15 17:32:14 +0500

    верстка основных компонентов

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,10 @@
 import "../scss/main.scss";
 import "./dropdown";
+import "./animateNumber";
 import "./diagram";
+
+// import Swiper styles
+// import "swiper/css";
+
+import "./swiper";
 import "../index.html";
---
+++ src/js/swiper.js
@@ -0,0 +1,40 @@
+import Swiper from "./swiper-bundle.min";
+function swiper(name, opts) {
+  return new Swiper(name, {
+    loop: true,
+    // autoplay: {
+    //   delay: 2500,
+    //   disableOnInteraction: false,
+    // },
+    spaceBetween: 16,
+            pagination: {
+      el: ".swiper-numbs",
+      bulletActiveClass: "numb-active",
+      bulletClass: "numb",
+      type: "bullets",
+      clickable: true,
+      renderBullet: function (index, className) {
+       return '<span class="' + className + '">' + (index + 1) + "</span>";
+      },
+    },
+    ...opts
+  });
+}
+class newSwiper extends swiper {}
+new newSwiper(".headerSwiper");
+new newSwiper(".readySwiper");
+new newSwiper(".newProject" , {
+  spaceBetween: 16,
+      pagination: {
+      el: ".swiper-dots",
+      bulletActiveClass: "dot-active",
+      bulletClass: "dot",
+      type: "bullets",
+      clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '"></span>';
+      },
+    }
+});
+new newSwiper(".newsSwiper");
+new newSwiper(".buttonsSwiper",{pagination:{}});
