3adf3c21806be557707e87f5a47077d2bd775218
Ralfik <developeraul@gmail.com>
2022-04-22 11:17:59 +0500

    правки по галереи и добавление lightbox

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,14 @@
 import "../scss/main.scss";
 import "./plugins/swiper";
 
+import GLightbox from "glightbox";
+const lightbox = GLightbox({
+  touchNavigation: false,
+  keyboardNavigation: false,
+  height: "auto",
+  loop: true,
+  autoplayVideos: true,
+});
 //pages
 // import "./pages/index"
 // import "./pages/requisites"
